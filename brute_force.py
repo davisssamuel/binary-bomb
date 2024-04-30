bc717c6d44cb8f328c6fecdc69abd096704ad634
Sam <77520156+davisssamuel@users.noreply.github.com>
2024-04-30 16:01:17 -0400

    updated brute_force.py

--- brute_force.py
+++ brute_force.py
@@ -6,7 +6,7 @@ def main():
     start_time = perf_counter()
     perm_set = product(range(0,10), repeat=6)
     for permutation in perm_set:
-        new_input = " ".join(mpa(str, permutation))
+        new_input = " ".join(map(str, permutation))
         curr_input = f"Verbosity leads to unclear, inarticulate things.\n1 2 4 8 16 32\n0 z 208\n10 37\n5 115\n{new_input}\n"
         process = subprocess.Popen(["./bomb"], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         output = process.communicate(input=curr_input.encode())[0]
